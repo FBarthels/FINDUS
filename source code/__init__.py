967c1353af35760b33f95427df13d3a759be502b
FBarthels <50399925+FBarthels@users.noreply.github.com>
2019-05-10 13:36:23 +0200

    Update __init__.py

--- source code/__init__.py
+++ source code/__init__.py
@@ -1,5 +1,5 @@
-from .rob import *
+from .findus import *
 from .assay import *
 from .peptide import *
 import os
-os.chdir('C:/python37/Lib/site-packages/rob')
+os.chdir('C:/python37/Lib/site-packages/findus')
